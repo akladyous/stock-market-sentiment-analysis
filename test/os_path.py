9941a81c34d04321afa0acc0e90bd3256b1a9f48
Boula Akladyous <akladyous@gmail.com>
2021-06-08 17:06:46 -0400

    test files

--- test/os_path.py
+++
@@ -1,8 +0,0 @@
-src_dir = os.path.join(os.getcwd(), os.pardir, 'src')
-sys.path.append(src_dir)
-
-path_to_holdout_data = os.path.join(os.pardir,
-                                    'data',
-                                    'TestData.csv')
-
-
--- test/test_selenium.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-DRIVER_PATH = './chromedriver/'
-driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
-driver.get('https://google.com')
